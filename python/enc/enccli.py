1a793f28e1d2085bb162c55100cbe2cb15f399c7
salman <salman_mak@yahoo.com>
2022-08-26 05:59:31 -0400

    enc2

--- python/enc/enccli.py
+++ python/enc/enccli.py
@@ -1,10 +1,10 @@
 import enckey
 
+enc_dec_keys = {}
 
 def enc_init():
-    commands = {'info': enc_info, 'load':enc_load, 'newkey':enc_newkey, 'save':enc_save}
+    commands = {'info': enc_info, 'load':enc_load, 'newkey':enc_newkey, 'save':enc_save, 'enc':enc_enc, 'dec':enc_dec}
     return commands
-
 
 def main_cli():
     commands = enc_init()
@@ -14,21 +14,31 @@ def main_cli():
         cmd = cmd_str.split()
         if cmd and cmd[0] == 'done':
             cli_end = True
-        if  cmd:
+        elif cmd and cmd[0] in commands:
             commands[cmd[0]](cmd[1:])
 
-
-def enc_save():
+def enc_save(*params):
     print('_____save')
 
-def enc_load():
+def enc_load(*params):
     print('_____load')
 
-def enc_newkey():
-    the_new_key = enckey.new_enc_key()
-    print('_____newkey')
+def enc_newkey(*params):
+    print('_____newkey', *params)
+    dict_enc,dict_dec = enckey.new_enc_key()
+    print(dict_enc)
+    print(dict_dec)
+    enc_dec_keys = {'key_name':''.join(params[0]),'enc_key':dict_enc,'dec_key':dict_dec}
+    print(enc_dec_keys)
 
-def enc_info():
+def enc_info(*params):
     print('____info')
+
+def enc_enc(*params):
+    print('____enc', *params)
+    enckey.do_enc(enc_dec_keys['enc_key'], ''.join(params[0]), ''.join(params[1]))
+
+def enc_dec(*params):
+    print('____dec')
 
 main_cli()
--- python/enc/enckey.py
+++ python/enc/enckey.py
@@ -1,3 +1,22 @@
+import string
+import random
 
 def new_enc_key():
-    return "bla bla"
+    letters = string.ascii_letters
+    letters_list = list(letters)
+    random.shuffle(letters_list)
+    shuffled = ''.join(letters_list)
+    dict_enc = {letters[i]:shuffled[i] for i in range(len(letters))}
+    dict_dec = {shuffled[i]:letters[i] for i in range(len(letters))}
+    return dict_enc,dict_dec
+
+def do_enc(enc_key ,plain_file1, cipher_file2):
+    cipher_letters = ""
+    with open (plain_file1, "r") as f1:
+        file_content = f1.read()
+        letters_list=list(file_content)
+        for letter in letters_list:
+            cipher_letters += enc_key[letter]
+    
+    with open (cipher_file2, "w") as f2:
+        f2.write(cipher_letters)
