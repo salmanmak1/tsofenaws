647e68dc87bdef2a972b80e275fddd7139ad181c
salman <salman_mak@yahoo.com>
2022-08-26 09:00:33 -0400

    enc3

--- python/enc/enccli.py
+++ python/enc/enccli.py
@@ -15,30 +15,39 @@ def main_cli():
         if cmd and cmd[0] == 'done':
             cli_end = True
         elif cmd and cmd[0] in commands:
-            commands[cmd[0]](cmd[1:])
+            commands[cmd[0]](*cmd[1:])
+        else:
+            print(f"The command: {cmd[0]} is not existed")
 
 def enc_save(*params):
-    print('_____save')
+    print('_____save', *params)
+    enckey.do_save(enc_dec_keys, params[0])
+    print(f"The current key {enc_dec_keys['key_name']} is saved into {params[0]} file")
 
 def enc_load(*params):
-    print('_____load')
+    print('_____load', *params)
+    enc_dec_keys = enckey.do_load(params[0])
+    print(f"The current key is replaced to {enc_dec_keys['key_name']} which is loaded from {params[0]} file")
 
 def enc_newkey(*params):
     print('_____newkey', *params)
     dict_enc,dict_dec = enckey.new_enc_key()
-    print(dict_enc)
-    print(dict_dec)
+    global enc_dec_keys
     enc_dec_keys = {'key_name':''.join(params[0]),'enc_key':dict_enc,'dec_key':dict_dec}
-    print(enc_dec_keys)
+    print(f"A new key: {params[0]} is created")
 
 def enc_info(*params):
-    print('____info')
+    print('____info', *params)
+    enckey.do_info(enc_dec_keys)
 
 def enc_enc(*params):
     print('____enc', *params)
     enckey.do_enc(enc_dec_keys['enc_key'], ''.join(params[0]), ''.join(params[1]))
+    print(f"The file {params[0]} is encrypted into {params[1]}")
 
 def enc_dec(*params):
-    print('____dec')
+    print('____dec', *params)
+    enckey.do_dec(enc_dec_keys['dec_key'], ''.join(params[0]), ''.join(params[1]))
+    print(f"The file {params[0]} is decrypted into {params[1]}")
 
 main_cli()
--- python/enc/enckey.py
+++ python/enc/enckey.py
@@ -1,8 +1,13 @@
+from curses import keyname
 import string
 import random
+import json
+import sys
+
+original_stdout = sys.stdout
 
 def new_enc_key():
-    letters = string.ascii_letters
+    letters = string.ascii_letters + ' ' + '\n'
     letters_list = list(letters)
     random.shuffle(letters_list)
     shuffled = ''.join(letters_list)
@@ -10,13 +15,48 @@ def new_enc_key():
     dict_dec = {shuffled[i]:letters[i] for i in range(len(letters))}
     return dict_enc,dict_dec
 
-def do_enc(enc_key ,plain_file1, cipher_file2):
+def do_enc(enc_key, plain_file1, cipher_file2):
     cipher_letters = ""
     with open (plain_file1, "r") as f1:
         file_content = f1.read()
         letters_list=list(file_content)
         for letter in letters_list:
             cipher_letters += enc_key[letter]
-    
-    with open (cipher_file2, "w") as f2:
+    with open (cipher_file2, "w+") as f2:
+        sys.stdout = f2
         f2.write(cipher_letters)
+        sys.stdout = original_stdout
+
+def do_dec(dec_key, cipher_file1, plain_file2):
+    plain_letters = ""
+    with open (cipher_file1, "r") as f1:
+        file_content = f1.read()
+        letters_list=list(file_content)
+        for letter in letters_list:
+            plain_letters += dec_key[letter]
+    with open (plain_file2, "w+") as f2:
+        sys.stdout = f2
+        f2.write(plain_letters)
+        sys.stdout = original_stdout
+
+def do_save(enc_dec_keys, keys_file_name):
+    with open (keys_file_name, "w+") as f1:
+        sys.stdout = f1
+        f1.write(json.dumps(enc_dec_keys))
+        sys.stdout = original_stdout
+
+def do_load(keys_file_name):
+    with open (keys_file_name, "r") as f1:
+        sys.stdout = f1
+        enc_dec_keys = json.loads(f1.read())
+        sys.stdout = original_stdout
+        return enc_dec_keys
+
+def do_info(enc_dec_keys):
+    print(f"Current key: {enc_dec_keys['key_name']}")
+    print("Encryption:")
+    print("".join(key for key in enc_dec_keys['enc_key']))
+    print("".join(enc_dec_keys['enc_key'][value] for value in enc_dec_keys['enc_key']))
+    print("Decryption:")
+    print("".join(key for key in enc_dec_keys['dec_key']))
+    print("".join(enc_dec_keys['dec_key'][value] for value in enc_dec_keys['dec_key']))
